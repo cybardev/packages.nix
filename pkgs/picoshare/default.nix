b182542ad41b1a09b164fdb8e1cc5c3dfe4d485f
cybardev <50134239+cybardev@users.noreply.github.com>
2025-12-11 02:49:32 -0400

    picoshare: prepare for nixpkgs

--- pkgs/picoshare/default.nix
+++ pkgs/picoshare/default.nix
@@ -1,21 +1,42 @@
-{ lib, pkgs, ... }: pkgs.buildGoModule rec {
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  nix-update-script,
+  litestream,
+  litestreamSupport ? false,
+  ...
+}:
+let
   pname = "picoshare";
   version = "1.5.1";
+in
+buildGoModule {
+  inherit pname version;
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "mtlynch";
     repo = "picoshare";
-    tag = "${version}";
+    tag = version;
     sha256 = "sha256-8mgrwnY0Y1CggAtc7BrAqC32+Wu82FQNhoK0ijM1RKw=";
   };
 
   vendorHash = "sha256-Wf0qKs/9XKnO2nx2KmTGPdqI0iFih30AGvOi94RPEjw=";
 
-  meta = with lib; {
-    description = "A minimalist, easy-to-host service for sharing images and other files";
-    home = "https://github.com/mtlynch/picoshare";
-    license = licenses.agpl3Only;
-    maintainers = [ maintainers.blokyk ];
+  buildInputs = if litestreamSupport then [ litestream ] else [ ];
+
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Minimalist, easy-to-host service for sharing images and other files";
+    homepage = "https://github.com/mtlynch/picoshare";
+    license = lib.licenses.agpl3Only;
     mainProgram = "picoshare";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    maintainers = with lib.maintainers; [
+      blokyk
+    ];
   };
 }
